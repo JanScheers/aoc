20500f5083f84bdc3c7e96a5d616979f1ae29428
Jan Scheers <jan@getmarple.io>
2023-12-22 11:45:18 +0100

    day12

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use aoc::{day12, get_day};
+use aoc::{day12, day13, get_day};
 
 fn main() {
-    //let input = day12::INPUT;
-    let input = &get_day(12);
-    //dbg!(day12::part_one(input));
-    //dbg!(day12::test(input));
-    dbg!(day12::part_two(input));
+    let input = day13::INPUT;
+    //let input = &get_day(12);
+    //dbg!(day13::parse(input));
+    dbg!(day13::part_one(input));
+    //dbg!(day13::part_two(input));
 }
