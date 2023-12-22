9f79c9c35e5409357d0e2778c0a792c5e5cceeb0
Jan Scheers <jan@getmarple.io>
2023-12-22 11:56:45 +0100

    smudge

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use aoc::{day12, day13, get_day};
 
 fn main() {
-    let input = day13::INPUT;
-    //let input = &get_day(12);
+    //let input = day13::INPUT;
+    let input = &get_day(13);
     //dbg!(day13::parse(input));
-    dbg!(day13::part_one(input));
-    //dbg!(day13::part_two(input));
+    //dbg!(day13::part_one(input));
+    dbg!(day13::part_two(input));
 }
