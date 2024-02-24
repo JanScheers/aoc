08e0b34fb0186871ca6e5d71a15bbbe3fbe0d7ea
Jan Scheers <jan@getmarple.io>
2024-02-24 10:40:39 +0100

    wip

--- src/day24.rs
+++ src/day24.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 pub const INPUT: &str = "19, 13, 30 @ -2,  1, -2
 18, 19, 22 @ -1, -1, -2
 20, 25, 34 @ -2, -2, -4
@@ -23,7 +25,6 @@ pub fn parse(input: &str) -> Vec<Hail> {
                         .collect::<Vec<i64>>()
                 })
                 .to_owned();
-
             Hail {
                 p: (p[0], p[1], p[2]),
                 v: (v[0], v[1], v[2]),
@@ -45,9 +46,17 @@ fn solve_t(a: &Hail, x: f64) -> f64 {
     (x - a.p.0 as f64) / a.v.0 as f64
 }
 
+fn does_intersect(a: &Hail, b: &Hail) -> bool {
+    let x = solve_x(a, b);
+    solve_t(a, x) >= 0.0 && solve_t(b, x) >= 0.0
+}
+/*
+let (va, vb) = (a.v.1 as f64 / a.v.0 as f64, b.v.1 as f64 / b.v.0 as f64);
+(a.p.0 as f64 * va - b.p.0 as f64 * vb - a.p.1 as f64 + b.p.1 as f64) / (va - vb)
+
+*/
 pub fn part_one(input: &str, low: f64, high: f64) -> i32 {
     let hail = parse(input);
-
     let mut count = 0;
     for i in 0..hail.len() - 1 {
         for j in i + 1..hail.len() {
@@ -66,4 +75,34 @@ pub fn part_one(input: &str, low: f64, high: f64) -> i32 {
         }
     }
     count
+}
+
+pub fn part_two(input: &str) -> (i64, i64, i64) {
+    let hail = parse(input);
+    let paths: Vec<HashSet<_>> = hail
+        .iter()
+        .skip(2)
+        .map(|h| path(h.clone()).collect())
+        .collect();
+    for a in path(hail[0].clone()).enumerate() {
+        for b in path(hail[1].clone()).enumerate() {
+            let ((t0, p0), (t1, p1)) = if a.0 <= b.0 { (a, b) } else { (b, a) };
+            let (t0, t1) = (t0 as i64, t1 as i64);
+            let dt = t1 - t0;
+            if dt == 0 {
+                continue;
+            }
+            let v = ((p1.0 - p0.0) / dt, (p1.1 - p0.1) / dt, (p1.2 - p0.2) / dt);
+            let p = (p0.0 - v.0 * t0, p0.1 - v.1 * t0, p0.2 - v.2 * t0);
+            let h: HashSet<_> = path(Hail { p, v }).collect();
+            if !paths.iter().any(|path| h.is_disjoint(path)) {
+                return p;
+            }
+        }
+    }
+    panic!("Hey");
+}
+
+fn path(h: Hail) -> impl Iterator<Item = (i64, i64, i64)> {
+    (0..1000).map(move |t| (h.p.0 + t * h.v.0, h.p.1 + t * h.v.1, h.p.2 + t * h.v.2))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,59 @@
+impl Solution {
+    pub fn find_all_people(n: i32, mut meetings: Vec<Vec<i32>>, first_person: i32) -> Vec<i32> {
+        meetings.sort_unstable_by_key(|m| m[2]);
+        let mut knows = vec![false; n as usize];
+        knows[0] = true;
+        knows[first_person as usize] = true;
+        let mut left = 0;
+        while left < meetings.len() {
+            let time = meetings[left][2];
+            let right = (left + 1..meetings.len())
+                .find(|i| meetings[*i][2] != time)
+                .unwrap_or(meetings.len());
+            let mut groups: Vec<Vec<usize>> = vec![];
+            for meet in &meetings[left..right] {
+                let (a, b) = (meet[0] as usize, meet[1] as usize);
+                let i = groups.iter().position(|v| v.contains(&a));
+                let j = groups.iter().position(|v| v.contains(&b));
+                match (i, j) {
+                    (None, None) => groups.push(vec![a, b]),
+                    (None, Some(j)) => groups[j].push(a),
+                    (Some(i), None) => groups[i].push(b),
+                    (Some(i), Some(j)) => {
+                        groups[i] = [groups[i].clone(), groups[j].clone()].concat();
+                        groups.remove(j);
+                    }
+                }
+                println!("{:?}", meet);
+                println!("{:?}", groups);
+            }
+            println!("{:?}", groups);
+            for group in groups {
+                if group.iter().any(|p| knows[*p]) {
+                    group.into_iter().for_each(|p| knows[p] = true);
+                }
+            }
+            left = right
+        }
+        knows
+            .into_iter()
+            .enumerate()
+            .filter_map(|(p, k)| if k { Some(p as i32) } else { None })
+            .collect()
+    }
+}
+
 fn main() {
     //let input = aoc::day24::INPUT;
-    let input = &aoc::get_day(24);
-    //println!("{:?}", aoc::day23::parse(input, false));
+    //let input = &aoc::get_day(24);
+    let meetings = [[1, 2, 2], [2, 1, 2], [2, 4, 2], [0, 3, 2]]
+        .map(|v| v.to_vec())
+        .to_vec();
+
+    println!("{:?}", Solution::find_all_people(5, meetings, 4));
     //println!("{:?}", aoc::day23::parse(input, true));
-    println!(
-        "part one: {:?}",
-        aoc::day24::part_one(input, 200000000000000.0, 400000000000000.0)
-    );
-    //println!("part two: {:?}", aoc::day23::part_two(input));
+    // println!("part one: {:?}", aoc::day24::part_one(input, 2e14, 4e14));
+    //println!("part two: {:?}", aoc::day24::part_two(input));
 }
+
+struct Solution;
