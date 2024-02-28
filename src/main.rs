20d441a1fc4a50631423be7b9df5eda75de159c9
Jan Scheers <jan@getmarple.io>
2024-02-28 12:02:05 +0100

    y22day4

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    //let input = aoc::y22::day2::INPUT;
-    let input = &aoc::y22::get_day(2);
+    //let input = aoc::y22::day3::INPUT;
+    let input = &aoc::y22::get_day(4);
 
     //println!("{:?}", aoc::day23::parse(input, true));
-    println!("part one: {:?}", aoc::y22::day2::part_one(input));
-    println!("part two: {:?}", aoc::y22::day2::part_two(input));
+    println!("part one: {:?}", aoc::y22::day4::part_one(input));
+    println!("part two: {:?}", aoc::y22::day4::part_two(input));
 }
