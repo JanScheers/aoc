1265f88d31578a207d05cc680f0b9c19df12c771
Jan Scheers <jan@getmarple.io>
2024-03-22 16:07:54 +0100

    day13

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
-    //let input = aoc::y22::day12::INPUT;
+    //let input = aoc::y22::day13::INPUT;
 
-    let input = &aoc::y22::get_day(12);
+    let input = &aoc::y22::get_day(13);
     //let v: Option<Box<ListNode>> = ListNode::serialize(&[1, 2, -3, 3, 1]);
 
     //println!("{:?}", aoc::day23::parse(input, true));
-    println!("part one: {:?}", aoc::y22::day12::part_one(input));
-    println!("part two: {:?}", aoc::y22::day12::part_two(input));
+    println!("part one: {:?}", aoc::y22::day13::part_one(input));
+    println!("part two: {:?}", aoc::y22::day13::part_two(input));
 }
