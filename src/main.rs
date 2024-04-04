1dca6004d0b60b48672aa27ea30979999aa9ae6d
Jan Scheers <jan@getmarple.io>
2024-04-04 13:31:25 +0200

    day15

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
-fn main() {
-    let input = aoc::y22::day14::INPUT;
+impl Solution {
+    pub fn max_depth(s: String) -> i32 {
+        let mut depth = 0;
+        s.chars()
+            .map(|c| {
+                match c {
+                    '(' => depth += 1,
+                    ')' => depth -= 1,
+                    _ => (),
+                };
+                depth
+            })
+            .max()
+            .unwrap()
+    }
+}
 
-    //let input = &aoc::y22::get_day(14);
+fn main() {
+    //let input = aoc::y22::day15::INPUT;
+
+    let input = &aoc::y22::get_day(15);
     //let v: Option<Box<ListNode>> = ListNode::serialize(&[1, 2, -3, 3, 1]);
 
-    //println!("{:?}", aoc::y22::day14::parse(input));
-    println!("part one: {:?}", aoc::y22::day14::part_one(input));
-    println!("part two: {:?}", aoc::y22::day14::part_two(input));
+    //println!("{:?}", aoc::y22::day15::parse(input));
+    println!("part one: {:?}", aoc::y22::day15::part_one(input, 2000000));
+    println!("part two: {:?}", aoc::y22::day15::part_two(input, 4000000));
 }
+struct Solution;
